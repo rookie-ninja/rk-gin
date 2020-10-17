9d40118d267117505ef857fa7ad58ecbdd078b09
dongxuny <dongxuny@gmail.com>
2020-10-18 02:50:05 +0800

    [rk-gin-interceptor] Add context [commit:dongxuny]

---
+++ context/context.go
@@ -0,0 +1,101 @@
+// Copyright (c) 2020 rookie-ninja
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+package rk_gin_inter_context
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/rookie-ninja/rk-gin-interceptor/logging/zap"
+	"github.com/rookie-ninja/rk-query"
+)
+
+const (
+	RequestIdKeyLowerCase = "requestid"
+	RequestIdKeyDash      = "request-id"
+	RequestIdKeyUnderline = "request_id"
+	RequestIdKeyDefault   = RequestIdKeyDash
+)
+
+// Add Key values to outgoing header
+// It should be used only for common usage
+func AddToOutgoingHeader(ctx gin.Context, key string, values string) {
+	header := ctx.Writer.Header()
+	header.Add(key, values)
+}
+
+// Add request id to outgoing metadata
+//
+// The request id would be printed on server's query log and client's query log
+func AddRequestIdToOutgoingHeader(ctx gin.Context) string {
+	requestId := GenerateRequestId()
+
+	if len(requestId) > 0 {
+		AddToOutgoingHeader(ctx, RequestIdKeyDefault, requestId)
+	}
+
+	return requestId
+}
+
+// Extract takes the call-scoped EventData from gin_zap middleware.
+func GetEvent(ctx gin.Context) rk_query.Event {
+	event, ok := ctx.Get(rk_gin_inter_logging.RKEventKey)
+	if !ok {
+		return rk_query.NewEventFactory().CreateEventNoop()
+	}
+
+	return event.(rk_query.Event)
+}
+
+func GetRequestIdsFromOutgoingHeader(ctx gin.Context) []string {
+	res := make([]string, 0)
+
+	res = append(res, ctx.Writer.Header().Values(RequestIdKeyDash)...)
+	res = append(res, ctx.Writer.Header().Values(RequestIdKeyUnderline)...)
+	res = append(res, ctx.Writer.Header().Values(RequestIdKeyLowerCase)...)
+
+	return res
+}
+
+func GetRequestIdsFromIncomingHeader(ctx gin.Context) []string {
+	res := make([]string, 0)
+
+	res = append(res, ctx.Request.Header.Values(RequestIdKeyDash)...)
+	res = append(res, ctx.Request.Header.Values(RequestIdKeyUnderline)...)
+	res = append(res, ctx.Request.Header.Values(RequestIdKeyLowerCase)...)
+
+	return res
+}
+
+// Generate request id based on google/uuid
+// UUIDs are based on RFC 4122 and DCE 1.1: Authentication and Security Services.
+//
+// A UUID is a 16 byte (128 bit) array. UUIDs may be used as keys to maps or compared directly.
+func GenerateRequestId() string {
+	// Do not use uuid.New() since it would panic if any error occurs
+	requestId, err := uuid.NewRandom()
+
+	// Currently, we will return empty string if error occurs
+	if err != nil {
+		return ""
+	}
+
+	return requestId.String()
+}
+
+// Generate request id based on google/uuid
+// UUIDs are based on RFC 4122 and DCE 1.1: Authentication and Security Services.
+//
+// A UUID is a 16 byte (128 bit) array. UUIDs may be used as keys to maps or compared directly.
+func GenerateRequestIdWithPrefix(prefix string) string {
+	// Do not use uuid.New() since it would panic if any error occurs
+	requestId, err := uuid.NewRandom()
+
+	// Currently, we will return empty string if error occurs
+	if err != nil {
+		return ""
+	}
+
+	return prefix + "-" + requestId.String()
+}
--- go.mod
+++ go.mod
@@ -5,10 +5,7 @@ go 1.14
 require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/validator/v10 v10.4.0 // indirect
-	github.com/golang/protobuf v1.4.2 // indirect
-	github.com/json-iterator/go v1.1.10 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/google/uuid v1.1.2
 	github.com/prometheus/client_golang v1.7.1
 	github.com/rookie-ninja/rk-prom v1.0.1
 	github.com/rookie-ninja/rk-query v1.0.6
